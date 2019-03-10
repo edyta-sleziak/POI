e06c363b610ab51956bd345668d4492e0732abce
LAPTOP-QQULPCJ7\edyta <edyta.sleziak@gmail.com>
2019-03-10 21:53:49 +0000

    Update to index.js and package.json

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const Hapi = require('hapi');
 const cloudinary = require('cloudinary');
 
 const server = Hapi.server({
-  port: 3000,
-  host: 'localhost'
+  port: process.env.PORT || 3000,
 });
 
 require('./app/models/db');
